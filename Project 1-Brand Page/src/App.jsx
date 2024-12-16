d923b40feb18d9f094987a405cb505921736df68
V-M-B <vmbvarun@gmail.com>
2024-12-16 20:42:18 +0530

    Nav Corrected

--- Project 1-Brand Page/src/App.jsx
+++ Project 1-Brand Page/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./App.css";
-import Navigation from './components/navigation';
+import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 export default function App() {
   
